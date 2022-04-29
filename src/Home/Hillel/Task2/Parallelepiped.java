5ea76b57534b184cd3dc2bf1a62a1ee59e0aca68
artempolyakov1987 <polyakov.art1987@gmail.cpm>
2022-04-29 22:34:26 +0300

    second task parallelepiped

---
+++ src/Home/Hillel/Task2/Parallelepiped.java
@@ -0,0 +1,22 @@
+package Home.Hillel.Task2;
+
+public class Parallelepiped {
+    public static void main(String[] args) {
+        double a = 13.6;
+        double b = 16.5;
+        double c = 21;
+        double volume = a * b * c;
+        double length = 4*(a+b+c);
+        float w = 48.42596F;
+        float d = 35.02942F;
+
+        System.out.printf("Volume of the parallelepiped = %.2f", volume);
+        System.out.println();
+        System.out.println("Length = " + length);
+        System.out.println();
+        System.out.printf("Width = " + w);
+        System.out.println();
+        System.out.printf("Duration = " + d);
+    }
+
+}
