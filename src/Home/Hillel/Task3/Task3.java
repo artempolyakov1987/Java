4e74de5f6e44fefb4268565af0f54141943b3d03
artempolyakov1987 <polyakov.art1987@gmail.cpm>
2022-05-03 23:45:55 +0300

    Fix Task3

--- src/Home/Hillel/Task3/Task3.java
+++ src/Home/Hillel/Task3/Task3.java
@@ -7,13 +7,13 @@ public class Task3 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
-        System.out.print("Input a number min:");
+        System.out.print("Enter min limit:");
         int min = in.nextInt();
-        System.out.print("Input a number max:");
+        System.out.print("Enter max limit:");
         int max = in.nextInt();
 
         int random = (int) ((Math.random() * ((max - min) + 1)) + min);
 
-        System.out.printf("Your number: %d \n", random);
+        System.out.printf("The random value is: %d \n", random);
     }
 }
