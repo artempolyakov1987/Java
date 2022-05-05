0c8d33c1518056200214450bf2ed32407942d298
artempolyakov1987 <polyakov.art1987@gmail.cpm>
2022-05-05 22:54:57 +0300

    Task4/1

--- src/Task4/Person.java
+++ src/Task4/Person.java
@@ -2,13 +2,13 @@ package Task4;
 
 public class Person {
     public static void main(String[] args) {
-        System.out.println(personInfo("Will", "Smith","New York","2936729462846"));
-        System.out.println(personInfo("Jackie", "Chan","Shanghai","12312412412"));
-        System.out.println(personInfo("Sherlock", "Holmes","London","2936729462568"));
+        System.out.println(personInfo("Will", "Smith", "New York", "2936729462846"));
+        System.out.println(personInfo("Jackie", "Chan", "Shanghai", "12312412412"));
+        System.out.println(personInfo("Sherlock", "Holmes", "London", "2936729462568"));
     }
 
     public static String personInfo(String firstName, String lastName, String city, String number) {
-     return "Call a citizen  " + firstName + "  " + lastName + "  from the city  " + city + "   possibly by number  " + number;
+        return "Call a citizen  " + firstName + "  " + lastName + "  from the city  " + city + "   possibly by number  " + number;
 
     }
 }
