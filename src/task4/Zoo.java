65c1235dd493dff58644095b79ce5484cf3b3719
artempolyakov1987 <polyakov.art1987@gmail.cpm>
2022-05-06 18:34:07 +0300

    Zoo

--- src/task4/Zoo.java
+++ src/task4/Zoo.java
@@ -1,9 +1,11 @@
+package task4;
+
 public class Zoo {
     public static void main(String[] args) {
         int beer = 2;
         int tiger = 5;
         int parrot = 20;
-        int elephant = 30;
+        int elephant = 3;
         int raccoon = 12;
         int porcupine = 29;
         int count = (int) countAnimals(beer, tiger, parrot, elephant, raccoon);
@@ -28,7 +30,7 @@ public class Zoo {
     }
 
     public static double countAnimalsNextYear6(int beer, int tiger, int parrot, int elephant, int raccoon, int porcupine) {
-        return (double) ((beer * 0.3 + beer) + (tiger * 0.2 + tiger) + (parrot * 0.4 + parrot) + (elephant * 0.05 + elephant) + (raccoon * 0.8 + raccoon) + (porcupine * 0.15 + porcupine));
+        return (double) (countAnimalsNextYear(beer, tiger, parrot, elephant, raccoon) + (porcupine * 0.15 + porcupine));
 
     }
 }
