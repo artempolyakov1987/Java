940fb788804903f1d1741d1355533f5cc334f4ca
artempolyakov1987 <polyakov.art1987@gmail.cpm>
2022-05-11 12:25:44 +0300

    Conditionals operators

---
+++ src/task5/DecimalPlace.java
@@ -0,0 +1,17 @@
+package task5;
+
+
+public class DecimalPlace {
+    public static void main(String[] args) {
+        double number1= doubleToDecimalPlaces(1444.4454444, 2);
+        System.out.println(number1);
+
+    }
+    private static double doubleToDecimalPlaces(double number, int countAfterPoint){
+        int number1 = (int)(Math.pow(10, countAfterPoint));
+        return (double) (Math.round(number*number1))/number1;
+
+
+
+    }
+}
